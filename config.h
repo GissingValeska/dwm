c96e8bc499f4bce374b083426cd4dada6b8a9651
GissValeska <3011371536@qq.com>
2022-03-18 10:54:56 +0800

    flameshot

--- config.h
+++ config.h
@@ -60,7 +60,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *rofi[]  = { "rofi","-show","drun", NULL };
-static const char *flameshot[]  = { "rofi","-show","drun", NULL };
+static const char *flameshot[]  = { "/home/smile/dwm/scripts/flameshot.sh", NULL };
 static const char *scktog[]  = { "/home/smile/dwm/scripts/sck-tog.sh", NULL };
 
 static Key keys[] = {
@@ -68,6 +68,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = rofi } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = scktog } },
+	{ MODKEY|ShiftMask,             XK_r,      spawn,          {.v = flameshot } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
